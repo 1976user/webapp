fac3d77a3a965e87bd420ec469ab223f09443ec2
1976user <1_976@mail.ru>
2022-02-22 13:26:03 +0400

    Fixed identation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.ts',
-	module: {
-		rules: [
-		{
-			test: /\.ts$/,
-			use: 'ts-loader',
-			exclude: /node_modules/
-		},
-		{
-			test: /\.html$/,
-			use: {
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]'
-				}
-			}
-		},
-		{
-			test: /\.css$/,
-			use: {
-				loader: 'file-loader',
-				options: {
-					name: 'css/[name].[ext]'
-				}
-			}
-		}]
-	},
-	resolve: {
-		extensions: ['.ts']
-	},
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'js/app.js'
-	}
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+    {
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    },
+    {
+      test: /\.html$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }
+    },
+    {
+      test: /\.css$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'css/[name].[ext]'
+        }
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.ts']
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'js/app.js'
+  }
 }
